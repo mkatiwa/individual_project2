d5f629dd77e75938a3786c68163f94c403101f48
Mawia <m.katiwa@alustudent.com>
2024-11-28 20:57:49 +0300

    assignment

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -62,7 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: TextField(
               controller: _searchController,
               decoration: const InputDecoration(
-                hintText: 'Search by Country name',
+                hintText: 'Search by Country Name',
                 border: OutlineInputBorder(),
                 prefixIcon: Icon(Icons.search),
               ),
